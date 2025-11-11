9520cdbfb6615dba30005272cc3bb14df18159c5
Kritt07 <dum98415@gmail.com>
2025-11-11 20:33:56 +0500

    Задание Formule

--- 04/Formula.asm
+++ 04/Formula.asm
@@ -1,1 +1,27 @@
 // RAM[4] = (RAM[0] * 3 + (RAM[1] | RAM[2])) & !RAM[3] + 11
+
+// D = RAM[1] | RAM[2]
+@1
+D=M
+@2
+A=M
+D=D|A
+
+// D += RAM[0] * 3
+@0
+D=D+M
+D=D+M
+D=D+M
+
+// D = D & !RAM[3]
+@3
+A=!M
+D=D&A
+
+// D += 11
+@11
+D=D+A
+
+// RAM[4] = D
+@4
+M=D
