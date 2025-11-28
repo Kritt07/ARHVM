3442d61d7d0e797ddddd17cee851f31a468a46d4
Kritt07 <dum98415@gmail.com>
2025-11-28 17:23:50 +0500

    Сделал часть HackTranslator.cs

--- Assembler/HackTranslator.cs
+++ Assembler/HackTranslator.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 using System.Reflection.Emit;
+using NUnit.Framework.Interfaces;
+using System.Collections;
+using Newtonsoft.Json.Linq;
 
 namespace Assembler
 {
@@ -55,7 +58,100 @@ namespace Assembler
         /// <returns>Строка, содержащее нули и единицы — бинарное представление ассемблерной инструкции, например, "1111000010100000"</returns>
         public string CInstructionToCode(string cInstruction)
         {
-            throw new NotImplementedException();
+            var result = new int[15] {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            var destCompJumpArray = cInstruction.Split('=', ';');
+            var dest = destCompJumpArray[0];
+            var comp = destCompJumpArray[1];
+            var jump = destCompJumpArray.Length > 2 ? destCompJumpArray[2] : "";
+
+            ParseDest(dest, result);
+            ParseCamp(comp, result);
+            ParseJump(jump, result);
+
+            return String.Join();
+        }
+
+        public static void ParseCamp(string comp, int[] result)
+        {
+            var aluInstruction = new int[6];
+
+            var compToBinary = new Dictionary<string, int[]>()
+            {
+                {"0", new[] {0, 1, 0, 1, 0, 1}},
+                {"1", new[] {1, 1, 1, 1, 1, 1}},
+                {"-1", new[] {0, 1, 0, 1, 1, 1}},
+                {"D", new[] {0, 0, 1, 1, 0, 0}},
+                {"A", new[] {0, 0, 0, 0, 1, 1}},
+                {"!D", new[] {1, 0, 1, 1, 0, 0,}},
+                {"!A", new[] {1, 0, 0, 0, 1, 1}},
+                {"-D", new[] {1, 1, 1, 1, 0, 0}},
+                {"-A", new[] {1, 1, 0, 0, 1, 1}},
+                {"D+1", new[] {1, 1, 1, 1, 1, 0}},
+                {"A+1", new[] {1, 1, 1, 0, 0, 1}},
+                {"D-1", new[] {0, 1, 1, 1, 0, 0}},
+                {"A-1", new[] {0, 1, 0, 0, 1, 1}},
+                {"D+A", new[] {0, 1, 0, 0, 0, 0}},
+                {"D-A", new[] {1, 1, 0, 0, 1, 0}},
+                {"A-D", new[] {1, 1, 1, 0, 0, 0}},
+                {"D&A", new[] {0, 0, 0, 0, 0, 0}},
+                {"D|A", new[] {1, 0, 1, 0, 1, 0}}
+            };
+
+            if (comp.Contains('M'))
+            {
+                result[12] = 1; 
+                comp.Replace('M', 'A');
+            }
+
+            foreach (var line in compToBinary)
+                if (line.Key == comp)
+                    aluInstruction = line.Value;
+
+            for (var i = 0; i < 6; i++)
+                result[i + 6] = aluInstruction[0];
+        }
+
+        public static void ParseDest(string dest, int[] result)
+        {
+
+            var destToBinory = new Dictionary<string, int[]>()
+            {
+                {"", new[] {0, 0, 0}},
+                {"M", new[] {0, 0, 1}},
+                {"D", new[] {0, 1, 0}},
+                {"MD", new[] {0, 1, 1}},
+                {"A", new[] {1, 0, 0}},
+                {"AM", new[] {1, 0, 1}},
+                {"AD", new[] {1, 1, 0}},
+                {"AMD", new[] {1, 1, 1}}
+            };
+
+            foreach (var line in destToBinory)
+                if (line.Key == dest)
+                    for (var i = 0; i < 3; i++)
+                        result[i + 3] = line.Value[i];
+        }
+
+        public static void ParseJump(string jump, int[] result)
+        {
+
+            var jumpToBinory = new Dictionary<string, int[]>()
+            {
+                {"", new[] {0, 0, 0}},
+                {"JGT", new[] {0, 0, 1}},
+                {"JEQ", new[] {0, 1, 0}},
+                {"JGE", new[] {0, 1, 1}},
+                {"JLT", new[] {1, 0, 0}},
+                {"JNE", new[] {1, 0, 1}},
+                {"JLE", new[] {1, 1, 0}},
+                {"JMP", new[] {1, 1, 1}}
+            };
+
+            foreach (var line in jumpToBinory)
+                if (line.Key == jump)
+                    for (var i = 0; i < 3; i++)
+                        result[i] = line.Value[i];
         }
     }
 }
--- Assembler/obj/Debug/net6.0/Assembler.AssemblyInfo.cs
+++ Assembler/obj/Debug/net6.0/Assembler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assembler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e44fe718d3dd1e18194192e53e67fc80481912a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93605d807a00d88fee3cf6f7a500ec9425874f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assembler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assembler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
